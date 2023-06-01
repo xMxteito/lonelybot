84cf031d28912946f855c220398cbf452215c1b7
xMxteito <67451090+xMxteito@users.noreply.github.com>
2023-06-01 13:40:49 -0500

    Create onReady.js

---
+++ events/onReady.js
@@ -0,0 +1,15 @@
+// —— Inicio
+const { Signale } = require('signale');
+const config = require('../config.js');
+const logger = new Signale({ scope: 'Discord' });
+
+module.exports = {
+	name: "ready",
+	once: true,
+
+	async execute(client) {
+        logger.success('Bot online!');
+        client.user.setPresence({ activities: [{ type: config.Discord.statusType, name: `${config.Discord.statusMsg}` }], status: 'idle' })
+        logger.success('Status Set!');
+	},
+};
